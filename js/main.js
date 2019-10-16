9ed1dfa18440c9da70e0141eea724d3b5d2a49f6
Lærke Høj Lange <hoej-lange@hotmail.com>
2019-10-16 08:41:13 +0200

    JS for animation started

--- js/main.js
+++ js/main.js
@@ -65,3 +65,33 @@ function showSlides(n, no) {
   }
   x[slideIndex[no]-1].style.display = "block";
 }
+
+//animation on image slideshow
+
+function startAnimation() {
+  let elements;
+  let windowHeight;
+
+  function init() {
+    elements = document.querySelectorAll('.slideshow-container');
+    windowHeight = window.innerHeight;
+  }
+
+  function checkPosition() {
+    for (var i = 0; i < elements.length; i++) {
+      let element = elements[i];
+      let positionFromTop = elements[i].getBoundingClientRect().top;
+
+      if (positionFromTop - windowHeight <= 0) {
+        element.classList.add('.slideup-animation');
+      }
+    }
+  }
+
+  window.addEventListener('scroll', checkPosition);
+  window.addEventListener('resize', init);
+
+  init();
+  checkPosition();
+};
+//
